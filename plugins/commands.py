50c2da2ae3e685e39a3cfd6daff2bf99810605d3
Psn <106792309+psnprasann@users.noreply.github.com>
2024-01-14 23:05:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -312,7 +312,7 @@ async def start(client, message):
                 chat_id=message.from_user.id,
                 file_id=file_id,
                 caption=f_caption,
-                protect_content=True if pre == 'filep' else False,
+                protect_content=True if pre == 'filep' else True,
                 reply_markup=(
                     InlineKeyboardMarkup(
                         [
@@ -384,7 +384,7 @@ async def start(client, message):
             msg = await client.send_cached_media(
                 chat_id=message.from_user.id,
                 file_id=file_id,
-                protect_content=True if pre == 'filep' else False,
+                protect_content=True if pre == 'filep' else True,
                 reply_markup=(
                     InlineKeyboardMarkup(
                         [
@@ -455,7 +455,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        protect_content=True if pre == 'filep' else False,
+        protect_content=True if pre == 'filep' else True,
         reply_markup=(
             InlineKeyboardMarkup(
                 [
