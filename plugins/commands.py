e2e205ea4b92fd29751956fa62bffac47daf2142
Psn <106792309+psnprasann@users.noreply.github.com>
2024-01-14 18:57:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -49,7 +49,7 @@ async def start(client, message):
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         m=await message.reply_sticker("CAACAgUAAx0CcOCC0wAD3mWjWqQoZGIw0X2YPM6wXQERWbNJAAInDAACeuAZVS3vPzcReuFPHgQ") 
-        await asyncio.sleep(1)
+        await asyncio.sleep(1000000)
         await m.delete()
         await message.reply_photo(
             photo=random.choice(PICS),
